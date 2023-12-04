9c72bbb3d3ca18f153c5c60e7606123f87bb1d2e
GidiMerken <gidi@ecbase.nl>
2023-12-04 14:38:42 +0100

    Gidi Test

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>ecBase Documentatie</span>,
+  logo: 
+  
+  <span>
+
+    ecBase Documentatie
+    
+    </span>
+    
+    ,
+
+
   project: {
     link: 'https://app.ecbase.nl',
   },
