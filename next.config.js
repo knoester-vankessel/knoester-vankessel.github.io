b0072e0a88f3ebd7d38ec656c0f235e23a53c04f
martijnmichel <martijn.michel@gmail.com>
2025-09-11 11:17:45 +0200

    deply

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
+/** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === "production";
+const assetPrefix = isProduction ? "/$REPO" : "";
+const titleSuffix = "– ecBase Documentatie"
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra({ output: 'export', images: { unoptimized: true }})
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  titleSuffix: titleSuffix,
+  assetPrefix,
+  basePath: assetPrefix,
+  output: "export",
+};
+
+module.exports = {
+  ...withNextra(),
+  ...nextConfig,
+};
